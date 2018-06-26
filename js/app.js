a4e4ca75a1b11e3e2f65f2759b9307d651ae23ff
Jake <32019706+JakaPresecnik@users.noreply.github.com>
2018-06-26 09:51:40 -0600

    second update
    removes: 
       - comented out the enemyCollision check, on developing purposes
    added:
       - 2 more levels and  chance to return to previuos screen
       - 1 more enemy (bat)
       - 2 keys and a treasure chest
       - a method that keys don't respawn when collected

--- js/app.js
+++ js/app.js
@@ -16,14 +16,19 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
-    if(player.level >= 7) {
-      if (this.x > 535) {
+    if(player.level === 7) {
+      if (this.x > 575) {
         this.speed = - this.speed;
-      }else if (this.x < -30) {
+      }else if (this.x < -70) {
         this.speed = - this.speed;
       }
-    }
-    else if(this.x > 700) {
+    } else if(player.level === 8 || player.level === 9) {
+      if (this.x > 373) {
+        this.speed = - this.speed;
+      }else if (this.x < -70) {
+        this.speed = - this.speed;
+      }
+    }else if(this.x > 700) {
       this.x = -100;
     }
     this.checkCollisions();
@@ -33,18 +38,20 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 Enemy.prototype.checkCollisions = function() {
-  if(this.x > player.x - 40 && this.x < player.x + 40) {
-    if(this.y > player.y - 40 && this.y < player.y + 40){
-      player.y = 383;
-      player.health -= 1;
-
-/*---->*/      console.log(player.health);
-/*---->*/      console.log(player.points);
-
+/*  if(this.x > player.x - 65 && this.x < player.x + 65) {
+    if(this.y > player.y - 50 && this.y < player.y + 50){
+      if(player.direction === 'up'){
+        player.y = 383;
+        player.x = 202;
+        player.health -= 1;
+      }else {
+        player.x = 202;
+        player.y = 55;
+        player.health -= 1;
+      }
     }
-  }
+  }*/
 }
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -53,8 +60,11 @@ var Player = function (x, y) {
   this.y = y;
   this.sprite = 'images/char-boy.png';
   this.level = 1;
-  this.health = 3;
+  this.health = 1;
   this.points = 0;
+  this.direction = 'up';
+  this.chestKey = 0;
+  this.gateKey = 0;
 }
 Player.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -78,7 +88,16 @@ Player.prototype.update = function () {
     allEnemies.forEach(function(enemy) {
       enemy.x = Math.floor(Math.random() * 699);
     });
+    this.direction = 'up';
     this.onLevelUp();
+  } else if(this.level === 8 || this.level === 9){
+    if(this.x === 101 && this.y >= 380) {
+        this.direction = 'down';
+        this.level -= 1;
+        this.x = 202;
+        this.y = 55;
+        this.onLevelUp();
+    }
   }
 };
 Player.prototype.onLevelUp = function() {
@@ -101,7 +120,7 @@ Player.prototype.onLevelUp = function() {
       });
       allEnemies.push(new Enemy(140, 220));
       break;
-    // This is where the game changes to sea.
+    // SEA
     case 5:
       gems = [new Gems(101, 170, 0), new Gems(303, 248, 3), new Gems(505, 170, 1)];
       obstacles = [new Obstacle(0, 83, 2), new Obstacle(202, 249, 2), new Obstacle(303, 332, 2), new Obstacle(505, 83, 4), new Obstacle(404, 249, 4), new Obstacle(303, 0, 4)];
@@ -120,13 +139,63 @@ Player.prototype.onLevelUp = function() {
         enemy.speed += 15;
       });
       break;
+    // BEACH
     case 7:
-      gems = [new Gems(0, 252, 1), new Gems(505, 248, 2)];
-      allEnemies.push(new Enemy(60, 100), new Enemy(60, 140));
-      obstacles = [new Obstacle(0, 55, 3), new Obstacle(101, 55, 0), new Obstacle(303, 55, 3), new Obstacle(505, 55, 3), new Obstacle(303, 140, 0), new Obstacle(0, 140, 3), new Obstacle(505, 300, 0), new Obstacle(505, 380, 3)]
+      gems = [new Gems(101, 170, 0), new Gems(0, 252, 1), new Gems(505, 248, 2)];
+      items = [new Item(508, 165, 0), new Item(202, 0, 1)];
+      allEnemies.push(new Enemy(140, 240), new Enemy(220, 300) );
+      obstacles = [
+        new Obstacle(0, 55, 3),
+        new Obstacle(101, 55, 0),
+        new Obstacle(303, 55, 3),
+        new Obstacle(505, 55, 3),
+        new Obstacle(303, 140, 0),
+        new Obstacle(0, 140, 3),
+        new Obstacle(505, 300, 0),
+        new Obstacle(505, 380, 3),
+        new Obstacle(404, 0, 5)]
       allEnemies.forEach(function(enemy) {
       enemy.sprite = 'images/enemy-crab.png';
-    });
+      });
+      break;
+    //CAVE 1st ENTERANCE
+    case 8:
+      gems = [new Gems(0, 320, 3)];
+      items = [new Item(202, 0, 1), new Item(101, 415, 2)];
+      if(this.gateKey === 0) {
+        items.push(new Item(303, 170, 3))
+      }
+      obstacles = [new Obstacle(101, 55, 6),
+        new Obstacle(0, 55, 6),
+        new Obstacle(303, 55, 6),
+        new Obstacle(101, 290, 6),
+        new Obstacle(0, 380, 6),
+        new Obstacle(303, 220, 6),
+        new Obstacle(404, 170, 6),
+        new Obstacle(404, 270, 6),
+        new Obstacle(303, 380, 6)
+      ];
+      allEnemies.forEach(function(enemy) {
+      enemy.sprite = 'images/enemy-bat.png';
+      });
+      break;
+    case 9:
+      gems = [new Gems(0, 330, 2),  new Gems(0, 410, 3)];
+      items = [new Item(101, 415, 2)];
+      if(this.chestKey === 0) {
+        items.push(new Item(0, 170, 4))
+      }
+      obstacles = [new Obstacle(101, 55, 6),
+      new Obstacle(202, 55, 6),
+      new Obstacle(303, 55, 6),
+      new Obstacle(0, 55, 6),
+      new Obstacle(101, 305, 6),
+      new Obstacle(303, 220, 6),
+      new Obstacle(404, 170, 6),
+      new Obstacle(404, 270, 6),
+      new Obstacle(303, 380, 6)
+    ];
+      break;
   }
 };
 
@@ -138,7 +207,6 @@ var Obstacle = function(x, y, i) {
 Obstacle.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
 Obstacle.prototype.update = function(key){
     if(this.x > player.x - 50 && this.x < player.x + 50) {
       if(this.y > player.y - 40 && this.y < player.y + 40){
@@ -168,8 +236,6 @@ var Gems = function(x, y, i) {
 Gems.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-
 Gems.prototype.update = function() {
   if(this.x > player.x - 40 && this.x < player.x + 40) {
     if(this.y > player.y - 40 && this.y < player.y + 40){
@@ -178,28 +244,54 @@ Gems.prototype.update = function() {
           player.health += 1;
           break;
         case gemsImages[1]:
-          player.points += 200;
+          player.points += 100;
           break;
         case gemsImages[2]:
-          player.points += 500;
+          player.points += 250;
           break;
         case gemsImages[3]:
-          player.points += 1000;
+          player.points += 500;
           break;
       }
       gems.splice(gems.indexOf(this), gems.indexOf(this) + 1);
-
-/*---->*/      console.log(player.health);
-/*---->*/      console.log(player.points);
     }
   }
 }
 
+var Item = function(x, y, i) {
+  this.x = x;
+  this.y = y;
+  this.sprite = itemImages[i];
+}
+Item.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Item.prototype.update = function() {
+  if(this.x > player.x - 40 && this.x < player.x + 40) {
+    if(this.y > player.y - 40 && this.y < player.y + 40){
+      switch (this.sprite) {
+        case itemImages[3]:
+          player.gateKey = 1;
+          items.splice(items.indexOf(this), items.indexOf(this) + 1);
+          break;
+        case itemImages[4]:
+          player.chestKey = 1;
+          items.splice(items.indexOf(this), items.indexOf(this) + 1);
+          break;
+      }
+    }
+  }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var gems = []
-var gemsImages = ['images/Heart.png', 'images/GemOrange.png', 'images/GemBlue.png', 'images/GemGreen.png']
+var gemsImages = [
+  'images/Heart.png',
+  'images/GemOrange.png',
+  'images/GemBlue.png',
+  'images/GemGreen.png',
+]
 var player = new Player(101, 383);
 var allEnemies = [
   new Enemy(60, 150),
@@ -213,12 +305,18 @@ var obstacleImages = [
   'images/bench.png',
   'images/signal-buoy.png',
   'images/palm-tree.png',
-  'images/sea-rock.png'
+  'images/sea-rock.png',
+  'images/cliff-hole-bars.png',
+  'images/Rock-dark.png'
 ]
-
-
-/*---->*/ console.log(player.health);
-console.log(player.noObstacle);
+var items = [];
+var itemImages = [
+  'images/treasure-chest.png',
+  'images/cliff-hole.png',
+  'images/way-out.png',
+  'images/gate-key.png',
+  'images/chest-key.png'
+];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
